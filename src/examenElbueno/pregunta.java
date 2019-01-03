4db62874a19d462d3c2d99fad7c63428e9ffb637
Melissa Juarez <melissa.juarez@michelada.io>
2019-01-03 13:27:45 -0600

    pedir nombre

--- src/examenElbueno/pregunta.java
+++ src/examenElbueno/pregunta.java
@@ -1,5 +1,33 @@
 package examenElbueno;
 
 public class pregunta {
+	int index;
+	String pregunta;
+	String respuestaCorrecta;
 
-}
+
+	public pregunta() {
+		pregunta = "";
+		respuestaCorrecta = "";
+	}
+
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	public String getRespuestaCorrecta() {
+		return respuestaCorrecta;
+	}
+	public void setRespuestaCorrecta(String respuestaCorrecta) {
+		this.respuestaCorrecta = respuestaCorrecta;
+	}
+
+	}
